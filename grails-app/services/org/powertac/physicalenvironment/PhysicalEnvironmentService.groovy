87c6c237697911eef553d254062f23ccddc58286
chrysopoulos18 <achryso@issel.ee.auth.gr>
2011-05-10 11:30:19 +0300

    Change fror Weather to Weather Report class

--- grails-app/services/org/powertac/physicalenvironment/PhysicalEnvironmentService.groovy
+++ grails-app/services/org/powertac/physicalenvironment/PhysicalEnvironmentService.groovy
@@ -1,17 +1,17 @@
 package org.powertac.physicalenvironment
 
-import org.powertac.common.interfaces.PhysicalEnvironment
-import org.powertac.common.Weather
 import org.powertac.common.Timeslot
+import org.powertac.common.WeatherReport
+import org.powertac.common.interfaces.PhysicalEnvironment
 
 class PhysicalEnvironmentService implements PhysicalEnvironment {
 
   static transactional = true
 
-  List<Weather> generateWeatherData(Timeslot currentTimeslot, List<Timeslot> targetTimeslots) {
+  List<WeatherReport> generateWeatherData(Timeslot currentTimeslot, List<Timeslot> targetTimeslots) {
     // Create and return dummy list
     List weatherData = []
-    weatherData.add(new Weather())
+    weatherData.add(new WeatherReport())
     log.info "generateWeatherData returns ${weatherData}"
     return weatherData
   }
